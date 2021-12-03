753492dfe92ce8176566a5b6ef5564f9550e81a5
Malinda <malindakpt@gmail.com>
2021-12-03 16:36:53 +0530

    Added redux dependancies and required files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import { Login } from './components/presentational/login/Login';
 import './config/firebase';
-// import { useEffect } from 'react';
 
 export const App = () => {
-  // useEffect(() => {
-
-  // }, [])
   return (
     <div className="App">
       <Login />
