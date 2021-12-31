53501645db1b7821001341646ad2317096efc4a2
Malinda <malindakpt@gmail.com>
2021-12-31 22:44:48 +0530

    Fixed lint issues

--- src/state/meta/metaSlice.ts
+++ src/state/meta/metaSlice.ts
@@ -1,6 +1,6 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { RootState, AppThunk } from '../store';
-import { fetchCount, fetchVehicleTypes } from '../../dataAPI/metaAPI';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+// import { RootState, AppThunk } from '../store';
+import { fetchVehicleTypes } from '../../dataAPI/metaAPI';
 import { MetaState } from '../../types/interfaces/MetaState';
 
 const initialState: MetaState = {
@@ -59,17 +59,16 @@ export const counterSlice = createSlice({
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectCount = (state: RootState) => state.counter.value;
 
 // We can also write thunks by hand, which may contain both sync and async logic.
 // Here's an example of conditionally dispatching actions based on current state.
-export const incrementIfOdd =
-  (amount: number): AppThunk =>
-  (dispatch, getState) => {
-    const currentValue = selectCount(getState());
-    if (currentValue % 2 === 1) {
-      dispatch(incrementByAmount(amount));
-    }
-  };
+// export const incrementIfOdd =
+//   (amount: number): AppThunk =>
+//   (dispatch, getState) => {
+//     const currentValue = selectCount(getState());
+//     if (currentValue % 2 === 1) {
+//       dispatch(incrementByAmount(amount));
+//     }
+//   };
 
 export default counterSlice.reducer;
--- src/state/user/userSlice.ts
+++ src/state/user/userSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { RootState, AppThunk } from '../store';
+// import { RootState, AppThunk } from '../store';
 import { UserState } from '../../types/interfaces/UserState';
 
 const initialState: UserState = {
