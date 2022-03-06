38b7b215f2fd084ff30af9c44ba3a18db2837aa5
Malinda <malindakpt@gmail.com>
2022-03-06 22:35:31 +0530

    Added more reducer functions

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import Login from './components/presentational/login/Login';
 import './config/firebase';
-import { useGetPokemonByNameQuery } from './state/services/pokemon';
+import { useGetPokemonByNameQuery, useGetPostQuery } from './state/services/pokemon';
 // import { useGetPokemonByNameQuery } from './dataAPI/query/PaymentApi';
 // import { isMetaAlreadyUpdated } from './config/util';
 // import { Entity } from './dataAPI/entity';
@@ -13,23 +13,17 @@ import { useGetPokemonByNameQuery } from './state/services/pokemon';
 export const App = () => {
   // const dispatch = useDispatch();
   const { data, error, isLoading } = useGetPokemonByNameQuery('bulbasaur');
+  const { data: data2, error: error2, isLoading: isLoading2 } = useGetPostQuery(12, {});
   useEffect(() => {
-    // const { data, error, isLoading } = useGetDataQuery();
-    // console.log(data);
-    // console.log(error);
-    // console.log(isLoading);
-    // dispatch(vehicleTypesAsync());
-    // isMetaAlreadyUpdated().then((isUpdated) => {
-    //   console.log('isMetaAlreadyUpdated:', isUpdated);
-    //   if (!isUpdated) {
-    //     dispatch(vehicleTypesAsync());
-    //   }
-    //   // addDocument(Entity.EXAMS, { a: 123 });
-    //   getDocumentsWithProps(Entity.EXAMS, {}).then((data) => console.log(data));
-    // });
+    console.log('useEffect');
   }, []);
   return (
     <div className="App">
+      <h1>My application2</h1>
+      <div>isLoading: {isLoading2}</div>
+      <div>error: {error2}</div>
+      <div>Data: {JSON.stringify(data2)}</div>
+
       <h1>My application</h1>
       <div>isLoading: {isLoading}</div>
       <div>error: {error}</div>
--- src/state/services/pokemon.ts
+++ src/state/services/pokemon.ts
@@ -1,18 +1,43 @@
 // Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { REHYDRATE } from 'redux-persist';
 // import { Pokemon } from './types';
 
 // Define a service using a base URL and expected endpoints
 export const pokemonApi = createApi({
   reducerPath: 'pokemonApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
+  extractRehydrationInfo: (action, { reducerPath }) => {
+    if (action.type === REHYDRATE) {
+      return action.payload[reducerPath];
+    }
+  },
   endpoints: (builder) => ({
     getPokemonByName: builder.query<any, string>({
       query: (name) => `pokemon/${name}`
+    }),
+    getPost: builder.query<any, number>({
+      // eslint-disable-next-line no-unused-vars
+      queryFn: (arg, queryApi, extraOptions, baseQuery) => {
+        if (arg <= 0) {
+          return {
+            error: {
+              status: 500,
+              statusText: 'Internal Server Error',
+              data: 'Invalid ID provided.'
+            }
+          };
+        }
+        const post: any = {
+          id: arg,
+          name: 'getRandomName()'
+        };
+        return { data: post };
+      }
     })
   })
 });
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetPokemonByNameQuery } = pokemonApi;
+export const { useGetPokemonByNameQuery, useGetPostQuery } = pokemonApi;
