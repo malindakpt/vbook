491d2eb12d91b27fdf4577caa65b7e3099e00364
Malinda Kumarasinghe <malindakpt@gmail.com>
2022-06-08 20:11:39 +0530

    FIxed compile issues

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,42 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action, createStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist';
 import apiReducer from '../features/counter/apiSlice';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, counterReducer);
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    counter: persistedReducer,
     api: apiReducer
-  }
+  },
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
 });
+
+// export const store = createStore(persistedReducer);
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
