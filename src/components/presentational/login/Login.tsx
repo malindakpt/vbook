b0762dfc03590b7edef13452779e1ceb0414b1c9
Malinda Kumarasinghe <malindakpt@gmail.com>
2022-06-11 04:25:40 +0530

    Added password login

--- src/components/presentational/login/Login.tsx
+++ src/components/presentational/login/Login.tsx
@@ -1,11 +1,15 @@
 import { GoogleLogin } from './google/GoogleLogin';
 import { FaceBookLogin } from './facebook/FaceBookLogin';
+import { FaceBookLogin2 } from './facebook2/FaceBookLogin';
+import { PasswordLogin } from './password/PasswordLogin';
 
 export const Login = () => {
   return (
     <div>
+      <FaceBookLogin2 />
       <FaceBookLogin />
       <GoogleLogin />
+      <PasswordLogin />
     </div>
   );
 };
