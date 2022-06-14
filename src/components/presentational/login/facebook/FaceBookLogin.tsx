fa7b0698cd45d8822dba23f80ed74fc2bbac8bc1
Malinda Kumarasinghe <malindakpt@gmail.com>
2022-06-15 01:15:12 +0530

    FB button UI

--- src/components/presentational/login/facebook/FaceBookLogin.tsx
+++ src/components/presentational/login/facebook/FaceBookLogin.tsx
@@ -1,4 +1,4 @@
-// import FacebookLogin from 'react-facebook-login';
+import classes from './FaceBookLogin.module.scss';
 import { FC } from 'react';
 import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
 import { User } from '../../../../entities/User';
@@ -12,17 +12,17 @@ export const FaceBookLogin: FC<Props> = ({ onLoginSuccess }) => {
     onLoginSuccess(new User(e.id, e.email, e.name));
   };
   return (
-    <div>
-      <FacebookLogin
-        appId="1180966182690354"
-        autoLoad={false}
-        fields="name,email,picture"
-        // onClick={componentClicked}
-        callback={responseFacebook}
-        render={(renderProps) => (
-          <button onClick={renderProps.onClick}>This is my custom FB button</button>
-        )}
-      />
-    </div>
+    <FacebookLogin
+      appId="1180966182690354"
+      autoLoad={false}
+      fields="name,email,picture"
+      // onClick={componentClicked}
+      callback={responseFacebook}
+      render={(renderProps) => (
+        <button className={classes.facebook} onClick={renderProps.onClick}>
+          Login with FaceBook
+        </button>
+      )}
+    />
   );
 };
