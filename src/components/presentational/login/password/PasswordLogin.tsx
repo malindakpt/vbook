03ec66823981455fefb7b40dd500b83218a2fe10
Malinda Kumarasinghe <malindakpt@gmail.com>
2022-06-15 02:51:31 +0530

    Added antd and modified login buttons

--- src/components/presentational/login/password/PasswordLogin.tsx
+++ src/components/presentational/login/password/PasswordLogin.tsx
@@ -5,8 +5,29 @@ import {
   signInWithEmailAndPassword
 } from 'firebase/auth';
 import { useState } from 'react';
+import { Collapse } from 'antd';
+import classes from './PasswordLogin.module.scss';
+import styled from 'styled-components';
+import { Input, Button } from 'antd';
 
+const NoPadding = styled.div`
+  .ant-collapse > .ant-collapse-item > .ant-collapse-header {
+    padding: 0px 10px;
+    background: #42904c;
+    color: white;
+    font-size: 20px;
+    height: 42px;
+    display: grid;
+    place-items: center;
+  }
+`;
 export const PasswordLogin = () => {
+  const { Panel } = Collapse;
+
+  const onChange = (key: string | string[]) => {
+    console.log(key);
+  };
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -62,22 +83,56 @@ export const PasswordLogin = () => {
   };
 
   return (
-    <div>
-      <div>Password login</div>
-      <div>
-        <input onChange={(e: any) => setEmail(e.target.value)} value={email}></input>
-      </div>
-      <div>
-        <input
-          onChange={(e: any) => setPassword(e.target.value)}
-          value={password}
-          type="password"></input>
-      </div>
-      <div>
-        <button onClick={createUser}>Create User</button>
-        <button onClick={resetPassword}>Reset Password</button>
-        <button onClick={login}>Login</button>
-      </div>
-    </div>
+    <NoPadding>
+      <Collapse
+        defaultActiveKey={['1']}
+        onChange={onChange}
+        bordered={false}
+        style={{ height: '42px', background: '#42904c' }}
+        className={classes.container}
+        expandIcon={() => <div></div>}>
+        <Panel key="1" header="Own Account" showArrow={false} className={classes.panel}>
+          <Input
+            placeholder="Enter Your Email"
+            onChange={(e: any) => setEmail(e.target.value)}
+            value={email}></Input>
+          <Input
+            placeholder="Enter Your Password"
+            onChange={(e: any) => setPassword(e.target.value)}
+            value={password}
+            type="password"></Input>
+          <Button block onClick={login} type="primary">
+            Login
+          </Button>
+          <Button block onClick={createUser}>
+            Create User
+          </Button>
+          <Button block onClick={resetPassword} danger>
+            Reset password for this email
+          </Button>
+        </Panel>
+      </Collapse>
+    </NoPadding>
   );
+  // return (
+  //   <Collapse defaultActiveKey={['1']}>
+  //     <div>
+  //       <div>Password login</div>
+  //       <div>
+  //         <input onChange={(e: any) => setEmail(e.target.value)} value={email}></input>
+  //       </div>
+  //       <div>
+  //         <input
+  //           onChange={(e: any) => setPassword(e.target.value)}
+  //           value={password}
+  //           type="password"></input>
+  //       </div>
+  //       <div>
+  //         <button onClick={createUser}>Create User</button>
+  //         <button onClick={resetPassword}>Reset Password</button>
+  //         <button onClick={login}>Login</button>
+  //       </div>
+  //     </div>
+  //   </Collapse>
+  // );
 };
