c7c1a610fa2562e57a824f100ee2b2af1ac8a521
Malinda Kumarasinghe <malindakpt@gmail.com>
2022-10-24 01:33:57 +0530

    db connected

--- server3/src/index.ts
+++ server3/src/index.ts
@@ -1,1 +1,20 @@
+import { Sequelize } from 'sequelize';
+
 console.log('start');
+
+const sequelize = new Sequelize('postgres', 'postgres', '123', {
+  host: 'localhost',
+  dialect:
+    'postgres' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+});
+
+const auth = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+auth();
