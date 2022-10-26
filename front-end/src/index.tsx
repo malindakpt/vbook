5dd4903e17ccef86193c4b02058064d6d2dc0e97
Malinda Kumarasinghe <malindakpt@gmail.com>
2022-10-26 07:24:16 +0530

    Aded miui dependancies

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -5,6 +5,11 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./state/store";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
