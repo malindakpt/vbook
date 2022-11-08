9d523e27263ae87d9a976d10d661b46aa9104984
Malinda Kumarasinghe <malindakpt@gmail.com>
2022-11-08 06:27:32 +0530

    Added useFormState hook

---
+++ fe/src/hooks/useFormState.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export const useFormState = <T,>(obj: T) => {
+    const [state, setState] = useState(obj);
+
+    const changeProperty = (prop: string, value: string | number) => {
+        setState((prev: any) => ({ ...prev, [prop]: value }));
+      };
+
+    const ret: [typeof state, typeof changeProperty] = [state, changeProperty];
+    return ret;
+    
+}
