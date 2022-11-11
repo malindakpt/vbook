7c28ad114d4c54f418952c46f0aa7e0102b655ea
Malinda Kumarasinghe <malindakpt@gmail.com>
2022-11-11 05:48:59 +0530

    Code refactored

--- fe/src/components/vehicle/createVehicle/createVehicle.container.tsx
+++ fe/src/components/vehicle/createVehicle/createVehicle.container.tsx
@@ -2,24 +2,26 @@ import { useCreateVehicleMutation } from "../../../state/api/vehicle.api";
 import { useAppSelector } from "../../../state/store";
 import { Vehicle } from "../../../types/Vehicle";
 import { ErrorComponent } from "../../error/error";
-import { CreateVehicle } from "./createVehicle"
+import { CreateVehicle } from "./createVehicle";
 
 export const CreateVehicleContainer = () => {
-    // const loading = useAppSelector((state) => state.app.addVehicle.loading);
-    const user = useAppSelector((state) => state.app.user);
-    const [ createVehicle, result ] = useCreateVehicleMutation()
+  // const loading = useAppSelector((state) => state.app.addVehicle.loading);
+  const user = useAppSelector((state) => state.app.user);
+  const [createVehicle, result] = useCreateVehicleMutation();
 
-    if(!user) {
-        return <ErrorComponent text="User N/A" />
-    }
-    
+  if (!user) {
+    return <ErrorComponent text="User N/A" />;
+  }
 
-    const handleCreateVehicle = (v: Vehicle) => {
-        console.log('vehicle', v);
-        createVehicle(v);
-    }
-   
-
-
-    return <CreateVehicle onCreateVehicle={handleCreateVehicle} loading={false} owner={user.identifier} />
-}
+  const handleCreateVehicle = (v: Vehicle) => {
+    console.log("vehicle", v);
+    createVehicle(v);
+  };
+  return (
+    <CreateVehicle
+      onCreateVehicle={handleCreateVehicle}
+      loading={false}
+      owner={user.identifier}
+    />
+  );
+};
