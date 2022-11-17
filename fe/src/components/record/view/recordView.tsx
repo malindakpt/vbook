76818b3c61974121f3c62c50fd29f0c65fdf3b22
Malinda Kumarasinghe <malindakpt@gmail.com>
2022-11-17 09:58:26 +0530

    Code refactored

--- fe/src/components/record/view/recordView.tsx
+++ fe/src/components/record/view/recordView.tsx
@@ -4,26 +4,11 @@ import {
   CardMedia,
   CardHeader,
   CardContent,
-  CardActionArea,
-  CardActions,
-  IconButton,
 } from "@mui/material";
-import EditIcon from "@mui/icons-material/Edit";
 import { FC } from "react";
 import { config } from "../../../config";
 import { Record } from "../../../types/Record";
 import { getServiceTypeLabel } from "../../../util/helper";
-
-// <div key={record.id}>
-//                   <div  onClick={() => onSelect(record)}>{record.date}</div>
-//                   <div>{record.desc}</div>
-//                   <div>{record.millage}</div>
-//                   <div>{record.type}</div>
-//                   <div>Img count: {record.imageCount}</div>
-//                   { record.imageCount > 0 && <img src={`${config.imageUrlPrefix}${record.id}-0.jpg`} alt="record"/>}
-//                   <Button onClick={() => onEdit(record)}>Edit</Button>
-//                   <Button onClick={() => onDelete(record)}>Delete</Button>
-//                 </div>
 interface Props {
   r: Record;
   onEdit: (r: Record) => void;
