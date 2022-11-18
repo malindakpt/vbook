4e21db4fe4e29153bd413c72e632915f22c5b701
Malinda Kumarasinghe <malindakpt@gmail.com>
2022-11-18 08:16:17 +0530

    UI refactored

--- fe/src/components/record/view/recordDetaledView.tsx
+++ fe/src/components/record/view/recordDetaledView.tsx
@@ -5,6 +5,7 @@ import {
   CardHeader,
   CardContent,
   Button,
+  Container,
 } from "@mui/material";
 import { FC } from "react";
 import { config } from "../../../config";
@@ -27,23 +28,25 @@ export const RecordDetailedView: FC<Props> = ({
   }
 
   return (
-    <Card sx={{ margin: 1 }}>
-      <CardHeader title={getServiceTypeLabel(r.type)} subheader={r.date} />
-      {r.imageCount > 0 && (
-        <CardMedia
-          sx={{
-            height: "200px",
-          }}
-          image={`${config.imageUrlPrefix}${r.id}-0.jpg`}
-        />
-      )}
-      <CardContent>
-        <Typography variant="body2">{`ODO Meter: ${r.millage}`}</Typography>
-        <Typography variant="body2">ODO Meter: {r.millage}</Typography>
-      </CardContent>
+    <Container maxWidth="md">
+      <Card sx={{ margin: 1 }}>
+        <CardHeader title={getServiceTypeLabel(r.type)} subheader={r.date} />
+        {r.imageCount > 0 && (
+          <CardMedia
+            sx={{
+              height: "300px",
+            }}
+            image={`${config.imageUrlPrefix}${r.id}-0.jpg`}
+          />
+        )}
+        <CardContent>
+          <Typography variant="body2">{`ODO Meter: ${r.millage}`}</Typography>
+          <Typography variant="body2">ODO Meter: {r.millage}</Typography>
+        </CardContent>
 
-      <Button onClick={() => onEdit(r)}>Edit</Button>
-      <Button onClick={() => onDelete(r)}>Delete</Button>
-    </Card>
+        <Button onClick={() => onEdit(r)}>Edit</Button>
+        <Button onClick={() => onDelete(r)}>Delete</Button>
+      </Card>
+    </Container>
   );
 };
