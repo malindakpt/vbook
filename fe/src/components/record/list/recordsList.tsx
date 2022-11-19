b82907d828b0bd1ddd5451212584c405f4f62ae9
Malinda Kumarasinghe <malindakpt@gmail.com>
2022-11-19 09:34:50 +0530

    withScroller refactored with generic types

--- fe/src/components/record/list/recordsList.tsx
+++ fe/src/components/record/list/recordsList.tsx
@@ -18,35 +18,13 @@ const ListRecords: FC<RecordsList> = ({
   onEdit,
   onDelete,
 }) => {
-  const [hasMore, setHasMore] = useState(true);
-  const [mergedData, setMergedData] = useState<Record[]>([]);
-  const [prevData, setPrevData] = useState<Record[]>([]);
-
-  if (hasMore && records && records.length === 0) {
-    setHasMore(false);
-  }
-
-  // TODO: Refactor this logic
-  if (
-    records &&
-    records.length > 0 &&
-    (prevData.length === 0 ? true : prevData[0].id !== records[0].id)
-  ) {
-    const allData = [...mergedData, ...records];
-    setMergedData(allData);
-    setPrevData(records);
-  }
-
   if (records) {
     return (
       <Container maxWidth="lg">
         <Grid container spacing={2} rowSpacing={4}>
-          {mergedData.map((record) => (
+          {records.map((record) => (
             <Grid key={record.id} xs={12} sm={6} md={4} item>
-              <RecordGridView
-                r={record}
-                onSelect={onSelect}
-              />
+              <RecordGridView r={record} onSelect={onSelect} />
             </Grid>
           ))}
         </Grid>
--- fe/src/hoc/withScroller.tsx
+++ fe/src/hoc/withScroller.tsx
@@ -1,21 +1,42 @@
 import { FC, useState } from "react";
 import InfiniteScroll from "react-infinite-scroller";
 import { config } from "../config";
+import { Record } from "../types/Record";
 
 interface Props {
-  loading: boolean,
-  onLoadMore: (newLastIndex: number) => void;
+  records: Record[];
+  loading: boolean;
+  onLoadMore: (nextLimit: number) => void;
 }
-export const withScroller = (Component: FC<any>) => {
-  return (props: any) => {
+export const withScroller = <T extends Props>(Component: FC<T>) => {
+  return (props: T) => {
+    const { records } = props;
+
+    const [prevReceivedData, setPreReceivedvData] = useState<Record[]>([]);
+    const [mergedData, setMergedData] = useState<Record[]>([]);
+    const [hasMore, setHasMore] = useState(true);
     const [lastIndex, setLastIndex] = useState(0);
+
+    if (hasMore && records && records.length === 0) {
+      setHasMore(false);
+    }
+
+    const isNewDataReceived =
+      records?.length > 0 &&
+      (prevReceivedData.length === 0 ? true : prevReceivedData[0].id !== records[0].id);
+
+    if (isNewDataReceived) {
+      const allData = [...mergedData, ...records];
+      setMergedData(allData);
+      setPreReceivedvData(records);
+    }
 
     const loadFunc = () => {
       if (!props.loading) {
         const newLastIndex = lastIndex + config.pageSize;
         setLastIndex(newLastIndex);
         props.onLoadMore(newLastIndex);
-        console.log('load more...');
+        console.log("loading upto ", newLastIndex);
       }
     };
 
@@ -24,13 +45,9 @@ export const withScroller = (Component: FC<any>) => {
         pageStart={0}
         loadMore={loadFunc}
         hasMore={props.records?.length > 0}
-        loader={
-          <div  key={0}>
-            Loading ...
-          </div>
-        }
+        loader={<div key={0}>Loading ...</div>}
       >
-        <Component {...props} />
+        <Component {...{ ...props, records: mergedData }} />
       </InfiniteScroll>
     );
   };
